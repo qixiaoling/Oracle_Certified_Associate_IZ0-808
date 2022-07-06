3e322af4471a3e408d9c7a61c5e853a797465c13
qixiaoling <xiaoling.qi@capgemini.com>
2022-07-06 14:40:46 +0200

    Yay

---
+++ src/BeforeTest/BeforeTest.java
@@ -0,0 +1,10 @@
+package BeforeTest;
+
+public class BeforeTest {
+    public static void main(String[] args) {
+        char c = 'c';
+        byte b = 100;
+//        c = (char)b;
+        b = (byte)c;
+    }
+}
